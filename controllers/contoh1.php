5063bfa8a2c60b6ce1ff3a036509024a7f71b64d
ramdhanni93 <128136751+ramdhanni93@users.noreply.github.com>
2023-03-28 01:07:13 +0700

    Add files via upload

---
+++ controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1?>Perkenalkan</h1>";
+        echo "Nama saya Ramdani
+              Saya tinggal di daerah Jakarta Pusat
+              Olahraga yang saya suka adalah berenang";
+    }
+}
